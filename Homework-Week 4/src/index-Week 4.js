70879be3c604a5aac27df1962871e03e8f65887d
robynabe <64428921+robynabe@users.noreply.github.com>
2020-05-22 09:39:14 -0700

    Transferred JS to its own file

--- Homework-Week 4/src/index-Week 4.js
+++ Homework-Week 4/src/index-Week 4.js
@@ -1,1 +1,74 @@
+let now = new Date();
+let div = document.querySelector("#date-time");
 
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let hour = now.getHours();
+let minute = now.getMinutes();
+let year = now.getFullYear();
+
+div.innerHTML = `${day}, ${month} ${date} ${hour}:${minute} ${year}`;
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function searchCity(city) {
+  let apiKey = "47aecf87524427e10968c44895784738";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function findWeather(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "47aecf87524427e10968c44895784738";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showTemperature);
+}
+function showCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", showCurrentLocation);
+
+let searchForm = document.querySelector("#search-location");
+searchForm.addEventListener("submit", findWeather);
+
+searchCity("Paris");
